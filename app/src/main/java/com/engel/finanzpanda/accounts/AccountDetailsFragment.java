5fbfcde7d55c24c25b80f31e3654e01f5fe9de48
Simon <66cheff66@gmail.com>
2022-12-12 23:29:30 +0100

    Started Account Details

---
+++ app/src/main/java/com/engel/finanzpanda/accounts/AccountDetailsFragment.java
@@ -0,0 +1,22 @@
+package com.engel.finanzpanda.accounts;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.engel.finanzpanda.R;
+
+public class AccountDetailsFragment extends Fragment {
+
+    public AccountDetailsFragment(){
+        super(R.layout.account_details_fragment);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
